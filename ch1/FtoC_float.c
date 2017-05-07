2d1054b2d2c8a61a051397610a17291d43ec6e89
mixelpixel <mixelpix@mac.com>
2017-05-07 01:20:04 -0400

    code layout

--- ch1/FtoC_float.c
+++ ch1/FtoC_float.c
@@ -11,12 +11,14 @@ int main()
   upper = 300;    /* upper limit */
   step = 20;      /* step size */
 
-  fahr = lower;
   printf("Fahr|Celsius\n------------\n");
+
+  fahr = lower;
   while (fahr <= upper) {
       celsius = (5.0/9.0) * (fahr-32.0);
       printf("%3.0f %6.1f\n", fahr, celsius);
       fahr = fahr + step;
   }
+
   return 0;
 }
