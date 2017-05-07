240480ead0fa4b834a87c69ffc2949ec5c0e77f1
mixelpixel <mixelpix@mac.com>
2017-05-07 00:40:09 -0400

    hello, world

--- ch.1/hello.c
+++ ch.1/hello.c
@@ -1,8 +1,4 @@
-// int main() & return 0 per modern C standard (I think)
-// see: http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf
-// see: http://stackoverflow.com/a/83763/5225057
-// see: https://stackoverflow.com/questions/43819099/how-to-not-invoke-warning-type-specifier-missing
-// differences between K&R and C99
+/* My first C program! */
                                //        per "The C programming Language" by
                                //   Brian W. Kernighan and Dennis M. Ritchie
                                //                Chapter 1.1 Getting Started
@@ -11,22 +7,22 @@
 int main()                     //           that received no argument values
 {                              //  statements of main are enclosed in braces
   printf("hello, world\n");    //         main calls library function printf
-  return 0;
+  return 0;                    // declaring function return type and setting return value not in K&R
 }                              //       to print this sequence of characters
                                //        \n represents the newline character
 
+/* C SYNTAX NOTE: these programs produce identical output:
 
-// C SYNTAX NOTE: these programs produce identical output:
-// #include <stdio.h>
-// main()
-// {printf("hello, ");printf("world");printf("\n");}
-//
-// or
-//
-// #include <stdio.h>
-// main()
-// {
-//   printf("hello, ");
-//   printf("world");
-//   printf("\n");
-// }
+#include <stdio.h>
+main()
+{printf("hello, ");printf("world");printf("\n");}
+
+or
+
+#include <stdio.h>
+main() {
+  printf("hello, ");
+  printf("world");
+  printf("\n");
+}
+*/
