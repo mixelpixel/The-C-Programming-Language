e56222ff0e1aef59192c697acaf7dff2a19c0576
mixelpixel <mixelpix@mac.com>
2017-05-12 01:56:43 -0400

    Exercise 1-6

--- ch1/Ex1-6.c
+++ ch1/Ex1-6.c
@@ -1,11 +1,12 @@
-/* Exercsise 1-6. Verify that the expression getchar() != EOF is 0 or 1. */
+/* Exercsise 1-6. Verify that the expression getchar() != EOF is 0 or 1.
 
-
-/*
-Prompts for an input and then captures a character
+Program prompts for an input and then captures a character
 from the keyboard. If EOF is signalled (typically through a
 control-D or control-Z character, though not necessarily),
-the program prints 0. Otherwise, it prints 1.
+the program evaluates a comparison of the keyboard
+charcter(s) (?) to EOF and prints 0 if it is FALSE that
+getchar() does not (!=) equal EOF. Otherwise, it prints 1
+because it is TRUE that getchar() does not equal EOF.
 
 If your input stream is buffered (and it probably is), then
 you will need to press the ENTER key before the program will
@@ -16,13 +17,22 @@ respond.
 
 int main()
 {
+
+  int c;
+
   printf("*******************************************************\n");
   printf("Press a key or a sequence of keys and then press ENTER.\n");
-  printf("Or, press ctrl+d, ctrl+z, or ctrl+c to exit.\n");
+  printf("Or, press ctrl+d. Press ctrl+z, or ctrl+c to EXIT.\n");
   printf("*******************************************************\n\n");
 
+  c = getchar();
+
+  printf("*****************************************************************\n");
+  printf("In this instance the value of EOF is %d and getchar() is %d\n\n", EOF, c);
   printf("*****************************************************************\n\
-In this instance the expression 'getchar() != EOF' evaluated to %d\n", getchar() != EOF);
+...and therefore the expression 'getchar() != EOF' evaluates to %d\n", c != EOF);
+  printf("*****************************************************************\n");
+  printf("...and 'getchar() = EOF' evaluates to %d\n", c = EOF);
   printf("*****************************************************************\n");
 
   return 0;
