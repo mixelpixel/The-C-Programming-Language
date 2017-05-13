2f5eac4dbc22612deaf38c991ba8c3d47dbb11cc
mixelpixel <mixelpix@mac.com>
2017-05-13 02:24:46 -0400

    1.5.1 character copying and printfing

---
+++ ch1/1.5.1_ex1-7.c
@@ -0,0 +1,12 @@
+/* Exercise 1-7. Write a program to print the value of EOF. */
+
+#include <stdio.h>
+
+/* prints the value of EOF, i.e. end-of-file */
+int main()
+{
+  printf("The value of EOF is %d\n\n", EOF);
+
+  return 0;
+}
+
