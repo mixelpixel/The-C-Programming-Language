5ed22c34e98d1376b9975fb2c6e57b98d83c12d7
mixelpixel <mixelpix@mac.com>
2017-05-13 06:19:35 -0400

    ex1-9 blanks

--- ch1/1.5.3_ex1-9.c
+++ ch1/1.5.3_ex1-9.c
@@ -4,27 +4,20 @@
 
 #include <stdio.h>
 
-/* Displays a count of the number of blanks, tabs and new lines in an input */
+/* Copy input to output, replace each string of blanks with one blank */
 int main(void)
 {
-    int blanks, tabs, new_lines;
+    int blanks;
     int c;
 
-    printf("This program will count how many blanks, tabs and new lines you've entered.\n");
+    printf("This program will count replace any string of blanks,with a single blank.\n");
     printf("To exit, use ctrl+d to insert an EOF after typing ENTER to exit the while loop.\n\n");
 
     blanks    = 0;
-    tabs      = 0;
-    new_lines = 0;
     while ( (c = getchar()) != EOF ) {
         if (c == ' ')
             ++blanks;
-        if (c == '\t')
-            ++tabs;
-        if (c == '\n')
-            ++new_lines;
     }
-    printf("You entered %d blanks, %d tabs, and %d new_lines.\n", blanks, tabs, new_lines);
 
     return 0;
 }
