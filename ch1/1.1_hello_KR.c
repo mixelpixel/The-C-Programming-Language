f63e4acd1e082ec075d6e445842655a7f452a71b
mixelpixel <mixelpix@mac.com>
2017-05-13 05:53:32 -0400

    K&R hello, world

---
+++ ch1/1.1_hello_KR.c
@@ -0,0 +1,7 @@
+#include <stdio.h>             // include information about standard library
+main()                         //              define a function called main
+                               //           that received no argument values
+{                              //  statements of main are enclosed in braces
+  printf("hello, world\n");    //         main calls library function printf
+}                              //       to print this sequence of characters
+                               //        \n represents the newline character
