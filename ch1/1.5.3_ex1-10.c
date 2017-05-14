fa1577620b9bf6c1f9a410d103f120353a02d8d1
mixelpixel <mixelpix@mac.com>
2017-05-14 02:33:29 -0400

    1.5.3 ex10 tabs, backspace, backslash

---
+++ ch1/1.5.3_ex1-10.c
@@ -0,0 +1,33 @@
+// Exercise 1-10. Write a program to copy its input to its output,
+//        replacing each tab by \t, each backspace by \b, and each
+//        backslash by \\. This makes tabs and backspaces visible
+//        in an unambiguous way.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    int finite_state;
+
+    while ( (c = getchar()) != EOF ) {
+        finite_state = 0;
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+            finite_state = 1;
+        }
+        if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+            finite_state = 1;
+        }
+        if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+            finite_state = 1;
+        }
+        if (finite_state == 0)
+            putchar(c);
+    }
+}
