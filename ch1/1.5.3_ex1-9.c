63d94a7bdda7a6dfa1ebd473f9c51d2b2643ff85
mixelpixel <mixelpix@mac.com>
2017-05-14 01:12:37 -0400

    1.5.3 ex 1-9 replace one or more blanks with a single blank

--- ch1/1.5.3_ex1-9.c
+++ ch1/1.5.3_ex1-9.c
@@ -4,20 +4,51 @@
 
 #include <stdio.h>
 
-/* Copy input to output, replace each string of blanks with one blank */
 int main(void)
 {
-    int blanks;
+    int blank_state, blank_count;
     int c;
 
-    printf("This program will count replace any string of blanks,with a single blank.\n");
-    printf("To exit, use ctrl+d to insert an EOF after typing ENTER to exit the while loop.\n\n");
+    printf("This program will replace any string of blanks with a single blank.\n");
+    printf("To exit the while loop, use ctrl+d to insert an EOF after typing ENTER.\n\n");
 
-    blanks    = 0;
-    while ( (c = getchar()) != EOF ) {
+    blank_state = 0;
+    blank_count = 0;
+    while ( (c = getchar()) != EOF )
+    {
         if (c == ' ')
-            ++blanks;
+        {
+            ++blank_count;
+            if (blank_state == 0)
+            {
+                blank_state = 1;
+                putchar(c);
+            }
+        }
+        if (c != ' ')
+        {
+            blank_state = 0;
+            putchar(c);
+        }
     }
+
+    printf("Total number of blanks: %d\n", blank_count);
 
     return 0;
 }
+
+// Look at your program as a machine that moves between different
+// states as it iterates over the input.
+//
+// It reads the input one character at a time.
+// If it sees anything other than a blank, it just prints the character it sees.
+// If it sees a blank, it shifts to a different state.
+// In that state, it prints one blank, and then doesn't print blanks if it sees them.
+// Then, it continues reading the input, but ignores all blanks it sees--
+// until it hits a character that isn't a blank,
+// at which point it shifts back to the first state.
+//
+// (This concept is called a finite state machine, by the way,
+// and a lot of theoretical computer science work has gone into
+// what they can and can't do. Wikipedia can tell you more,
+// though in perhaps more complicated detail than you're looking for. ;))
